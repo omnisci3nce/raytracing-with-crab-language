67a7f155df375d62e3029919213989f5e3c71751
Omniscient <17525998+omnisci3nce@users.noreply.github.com>
2023-02-05 13:20:41 +1100

    sphere

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,19 @@ const ASPECT_RATIO : f64 = 16.0 / 9.0;
 const IMAGE_WIDTH : i32 = 400;
 const IMAGE_HEIGHT : i32 =(IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
 
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+  let oc = r.origin - *center;
+  let a = Vec3::dot(&r.direction, &r.direction);
+  let b = Vec3::dot(&oc, &r.direction) * 2.0;
+  let c = Vec3::dot(&oc, &oc) - (radius * radius);
+  let discriminant = b * b -  (a * c) * 4.0;
+  return discriminant > 0.0;
+}
+
 fn ray_color(r: &Ray) -> Color {
+  if hit_sphere(&Point3{x:0.0, y:0.0, z:-1.0}, 0.5, r) {
+    return Color{x:1.0,y:0.0,z:0.0};
+  }
   let unit_direction = r.direction.unit_vector();
   let t = 0.5 * (unit_direction.y + 1.0);
   Color{x: 1.0, y:1.0,z:1.0} * (1.0 - t) + Color{x:0.5,y:0.7,z:1.0} * t
